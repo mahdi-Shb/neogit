3f166521617c99d2ee6f346fbfa9ecfdf0509946
mahdishb <mahdishb2006@gmail.com>
2024-01-28 17:51:56 +0330

    test2

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "header.h"
 int main(int argc, char const *argv[]){
-    for (int i = 0; i < argc; i++){
+    printf("ridam dahanet");
+    for (int i = 3; i < argc; i++){
         printf("%s ", argv[i]);
     }
     return 0;
