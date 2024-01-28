a2cc70e7cce78ed7f56af0d8a63ecee8dda29e8a
mahdishb <mahdishb2006@gmail.com>
2024-01-28 17:26:23 +0330

    testingcommit

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "header.h"
 int main(int argc, char const *argv[]){
-    printf("aa");
+    for (int i = 0; i < argc; i++){
+        printf("%s ", argv[i]);
+    }
     return 0;
 }
